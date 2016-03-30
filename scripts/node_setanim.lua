1e15f75fd2aacc0c5df6de0d8ba390edb183605a
gamrguy <gamrguy@github.com>
2016-03-29 20:34:51 -0400

    added swim animation button

--- scripts/node_setanim.lua
+++ scripts/node_setanim.lua
@@ -1,15 +1,24 @@
-local anim
+local on = false
 
-function init(me, a)
-	anim = a
+function init(me)
+	node_setCursorActivation(me, true)
 end
 
 function update(me, dt)
-	if(not STOPPED) then
-		node_setCursorActivation(me, true)
+	if(on) then
+		entity_setState(getNaija(), 1234)
+		if(entity_getAnimationName(getNaija()) ~= "swim") then
+			entity_animate(getNaija(), "swim", -1)
+		end
 	end
 end
 
 function activate(me)
-	entity_animate(getNaija(), "swim", -1)
+	if(not on) then
+		on = true
+	else
+		entity_idle(getNaija())
+		entity_setRiding(getNaija(), getNaija())
+		on = false
+	end
 end

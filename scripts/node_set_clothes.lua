ac9438872aaec99e01694b3f2d5c338c9680570d
Davenport <1533709@fcps.edu>
2016-04-04 12:24:11 -0400

    added hover text

--- scripts/node_set_clothes.lua
+++ scripts/node_set_clothes.lua
@@ -1,11 +1,44 @@
 if v == nil then v = {} end
 
 function init(me)
+	local t={} ; local i=1
+	for str in string.gmatch(node_getName(me), "([^".." ".."]+)") do
+		t[i] = str
+		i = i + 1
+	end
+	
+	v.set = t[2]
+	
+	if(v.set == "naija2") then
+		v.text = createBitmapText("Naija", 1500, 0, 0)
+	elseif(v.set == "energyform") then
+		v.text = createBitmapText("Energy", 1500, 0, 0)
+	elseif(v.set == "veggie") then
+		v.text = createBitmapText("Nature", 1500, 0, 0)
+	elseif(v.set == "cc") then
+		v.text = createBitmapText("Crab", 1500, 0, 0)
+	elseif(v.set == "etc") then
+		v.text = createBitmapText("Krotite", 1500, 0, 0)
+	elseif(v.set == "mithalas_girl1") then
+		v.text = createBitmapText("Mithalan 1", 1500, 0, 0)
+	elseif(v.set == "mithalas_girl2") then
+		v.text = createBitmapText("Mithalan 2", 1500, 0, 0)
+	elseif(v.set == "mithalan") then
+		v.text = createBitmapText("Princess", 1500, 0, 0)
+	else
+		v.text = createBitmapText(v.set, 1500, 0, 0)
+	end
 end
 
 function update(me, dt)
 	if(not STOPPED) then
 		node_setCursorActivation(me, true)
+	end
+	
+	if(node_isPositionIn(me, getMouseWorldPos())) then
+		text_setPosition(v.text, getMouseWorldPos())
+	else
+		text_setPosition(v.text, 0, 0)
 	end
 end
 
